3b7002a300c49901455cfce92ddaf88c8dd77805
michelef <michele.franzoni@muscope.com>
2025-05-09 08:46:45 +0200

    Added an init.py file to reuse functions and renamed files accordingly (no file should start with a number

---
+++ _7.py
@@ -0,0 +1,9 @@
+##########################################################PROBLEM###################################################
+
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10,001st prime number?
+
+### #####################################################SOLUTION#################################################
+
+import gcd_pair_iterative_mod
+
---
+++ __init__.py
@@ -0,0 +1,1 @@
+from _5.py import gcd_pair_iterative_mod
