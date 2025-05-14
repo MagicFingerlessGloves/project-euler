6877b02291b51c7febaef9ace1abf2703213188a
michelef <michele.franzoni@muscope.com>
2025-05-14 08:24:38 +0200

    Solved problem 7

--- _7.py
+++ _7.py
@@ -5,5 +5,18 @@
 
 ### #####################################################SOLUTION#################################################
 
-import gcd_pair_iterative_mod
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
+prime_count = 0
+start = 1
+while prime_count < 10001:
+    if is_prime(start):
+        prime_count += 1
+    start += 1
+print(start-1)
